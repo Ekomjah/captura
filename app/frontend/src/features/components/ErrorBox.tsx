0576f244024736a9a0d00e7d754a2b3a5fe88d4f
Ekomjah <ekomjahedet@gmail.com>
2026-06-08 19:54:16 +0100

    fix: correct typo in class name for ErrorBox component

--- app/frontend/src/features/components/ErrorBox.tsx
+++ app/frontend/src/features/components/ErrorBox.tsx
@@ -16,7 +16,7 @@ export default function ErrorBox({ message, queryKey }: ErrorBoxProps) {
       role="alert"
     >
       <p className="font-roboto-mono font-bold">Error loading assets</p>
-      <p className="text-sm text-muted-foreground mb-2" aria-live="polite">
+      <p className="ttext-muted-foreground mb-2" aria-live="polite">
         {message}
       </p>
       <Button
